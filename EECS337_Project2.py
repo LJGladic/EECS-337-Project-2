30f1c72e28635ab9df632bf8b73bb6357825721b
LJGladic <lukasgladic2017@u.northwestern.edu>
2019-02-26 17:46:59 -0600

    Commited intial files
    Made the base file for the project as well as a README.txt

---
+++ EECS337_Project2.py
@@ -0,0 +1,2 @@
+# EECS 337 Project 2 Group 18
+# Written by Andrew Bosset, Lukas J. Gladic, Julie Kim and Joshua Koo
---
+++ README.txt
@@ -0,0 +1,3 @@
+EECS 337 Project 2 Group 18
+Team Members: Andrew Bosset, Lukas J. Gladic, Julie Kim, Johsua Koo
+
