eed9538ece37dcf78208e3f1d0c8dcb2ed66260a
LJGladic <lukasgladic2017@u.northwestern.edu>
2019-02-26 18:03:04 -0600

    Update EECS337_Project2.py

--- EECS337_Project2.py
+++ EECS337_Project2.py
@@ -1,2 +1,8 @@
 # EECS 337 Project 2 Group 18
 # Written by Andrew Bosset, Lukas J. Gladic, Julie Kim and Joshua Koo
+
+import requests
+
+requested_recipe = requests.get('https://www.allrecipes.com/')
+print(requested_recipe)
+
