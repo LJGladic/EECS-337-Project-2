4f873325ea71fd3f668ee6e43c7878917ae941d4
LJGladic <lukasgladic2017@u.northwestern.edu>
2019-02-26 19:00:34 -0600

    Update EECS337_Project2.py
    Made the request fecther more robust

--- EECS337_Project2.py
+++ EECS337_Project2.py
@@ -5,14 +5,27 @@ import requests
 
 while True:
     requested_recipe_webpage = ''
+    requested_recipe = ''
     user_response = input('Do you have a recipe number? (y/n): ')
     if user_response == 'y':
-        recipe_number = input('Please enter the recipe number: ')
-        requested_recipe_webpage = 'https://www.allrecipes.com/recipe/'
-        requested_recipe_webpage += recipe_number
+        while True:
+            recipe_number = input('Please enter the recipe number: ')
+            requested_recipe_webpage = 'https://www.allrecipes.com/recipe/'
+            requested_recipe_webpage += recipe_number
+            requested_recipe = requests.get(requested_recipe_webpage)
+            if requested_recipe.status_code == 200:
+                break
+            else:
+                print("There was an error fetching that recipe number, please make sure it is correct")
         break
     elif user_response == 'n':
-        requested_recipe_webpage = input('Please enter any allrecipes.com recipe URL: ')
+        while True:
+            requested_recipe_webpage = input('Please enter any allrecipes.com recipe URL: ')
+            requested_recipe = requests.get(requested_recipe_webpage)
+            if requested_recipe.status_code == 200:
+                break
+            else:
+                print('There was an error fetching the recipe, please make sure the URL was correct')
         break
     else:
         print('Input not valid, please try again.')
