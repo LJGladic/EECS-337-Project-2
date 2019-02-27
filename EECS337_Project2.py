9f24028f2d93ccffa770ca92b8840cf2b733fbc9
LJGladic <lukasgladic2017@u.northwestern.edu>
2019-02-26 18:37:58 -0600

    Made a very basic recipe requester
    Implemented a real simple recipe requester to send the given requets to allrecipes.com

--- EECS337_Project2.py
+++ EECS337_Project2.py
@@ -3,6 +3,18 @@
 
 import requests
 
-requested_recipe = requests.get('https://www.allrecipes.com/')
-print(requested_recipe)
+while True:
+    requested_recipe_webpage = ''
+    user_response = input('Do you have a recipe number? (y/n): ')
+    if user_response == 'y':
+        recipe_number = input('Please enter the recipe number: ')
+        requested_recipe_webpage = 'https://www.allrecipes.com/recipe/'
+        requested_recipe_webpage += recipe_number
+        break
+    elif user_response == 'n':
+        requested_recipe_webpage = input('Please enter any allrecipes.com recipe URL: ')
+        break
+    else:
+        print('Input not valid, please try again.')
 
+requested_recipe = requests.get(requested_recipe_webpage)
