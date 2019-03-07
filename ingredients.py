53966a9e6d4b26e3f0e7d555e078240e09ea3ed8
acbosset <andrewbosset2019@u.northwestern.edu>
2019-03-07 02:28:52 -0600

    fixes () problem

--- ingredients.py
+++ ingredients.py
@@ -2,6 +2,10 @@ import numpy as np
 import re
 import nltk
 
+# good testers 7000
+# problems temp of water and decimals
+# remove anything with parentheses around it
+
 my_unit_list = [
     "teaspoon",
     "teaspoons",
@@ -74,10 +78,16 @@ my_unit_list = [
 preparations_list = [
     'drained',
     'chopped',
+    'softened'
 ]
 
+# get from text file
 descriptors_list = [
-
+    "room",
+    "temperature",
+    "chopped",
+    "diced",
+    "sliced"
 ]
 
 stop_words = [
@@ -86,7 +96,7 @@ stop_words = [
 ]
 
 
-print(stop_words)
+# print(stop_words)
 
 
 # potentially remove anything after or
@@ -97,41 +107,51 @@ def parse_ingredients(ingredients):
         quantity = ''
         measurement = ''
         name = ''
-        descriptor = ''
-        preperation = ''
+        descriptors = []
+        preperations = []
+
+        # adds parentheses to descriptors
+        desc = re.findall("\(.*?\)", i)
+        for d in desc:
+            if d != None:
+                descriptors.append(d)
+                i = i.replace(d, '')
         tokens = [t.lower() for t in i.split() if i.lower().replace('\(', '') not in stop_words]
         # find quantity
+        print(tokens)
         quantity = re.search("(-?(\d+)\s?((.\d+)?))", i)
         if quantity != None:
             quantity = str(quantity.group().strip())
             tokens.remove(quantity)
-            print (quantity)
+            # print (quantity)
 
         for t in tokens:
             # finding measurement
             if t in my_unit_list:
                 measurement = t
                 tokens.remove(t)
-                print(t)
+                # print(t)
             elif t in descriptors_list:
-                descriptor = t
+                descriptors.append(t)
                 tokens.remove(t)
-                print(t)
+                # print(t)
             elif t in preparations_list:
                 preparation = t
                 tokens.remove(t)
-                print(t)
+                # print(t)
             elif t in stop_words:
                 tokens.remove(t)
         name = " ".join(tokens).strip()
         print(name)
-        print(tokens)
+        # print(tokens)
 
         ingredient = {}
-        ingredient['name'] = ''
-        ingredient['quantity'] = 0
-        ingredient['measurement'] = ''
-        ingredient['descriptor'] = ''
+        ingredient['name'] = name
+        ingredient['quantity'] = quantity
+        ingredient['measurement'] = measurement
+        ingredient['descriptor'] = " ".join(descriptors).strip()
+        ingredient['preperations'] = " ".join(preperations).strip()
         ingredients_list.append(ingredient)
+        print(ingredient)
 # ingredient['prep'] = ''
     return ingredients_list
