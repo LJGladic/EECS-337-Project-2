1fdc55789d578b3989739d732c1e0af7a08ff236
LJGladic <lukasgladic2017@u.northwestern.edu>
2019-03-11 17:20:59 -0500

    Create italian.py
    Created the lists for italian transforms

---
+++ italian.py
@@ -0,0 +1,95 @@
+# Lists for transforming to Italian food
+
+italian_meats = [
+    'italian sausage',
+    'chicken',
+    'italian meatballs'
+]
+
+italian_fish = [
+    'european bass',
+    'tilapia',
+    'lobster',
+    'shrimp'
+]
+
+italian_herbs_and_spices = [
+    'thyme',
+    'oregano',
+    'rosemary',
+    'paprika',
+    'cilantro',
+    'basil',
+    'italian seasoning'
+]
+
+italian_cheeses = [
+    'asiago',
+    'parmesan',
+    'provolone',
+    'mozzarella',
+    'romano'
+]
+
+other_meats = [
+    'beef',
+    'beef chuck',
+    'ground beef',
+    'flank steak',
+    'corned beef',
+    'pot roast',
+    'steak',
+    'veal',
+    'sausage',
+    'chorizo',
+    'bacon',
+    'pork',
+    'ground pork',
+    'duck',
+    'foie gras',
+    'goose',
+    'turkey'
+]
+
+other_fish = [
+    'clam',
+    'crab',
+    'crawfish',
+    'fish',
+    'tuna',
+    'catfish',
+    'cod',
+    'mackerel',
+    'mahi mahi',
+    'pike',
+    'pollock',
+    'salmon',
+    'swordfish',
+    'trout',
+    'tuna',
+    'scallop',
+    'squid'
+]
+
+other_herbs_and_spices = [
+    'allspice',
+    'avocado leaf',
+    'bay leaf',
+    'caraway',
+    'cayenne pepper',
+    'chili pepper',
+    'chili powder',
+    'chives',
+    'coriander',
+    'curry',
+    'dill',
+    'ginger',
+    'jasmine',
+    'lemongrass',
+    'parsley',
+    'pepper',
+    'saffron',
+    'sage',
+    'tarragon'
+]
+
